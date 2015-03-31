cf4ad953ba032eb2310456f61da915fa18f567dc
Shortcut Media AG <account@shortcutmedia.com>
2015-03-31 11:14:56 -1000

    Add empty SDK module and dependency.

---
+++ src/androidTest/java/com/scm/shortcutreadersdk/ApplicationTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014 Shortcut Media AG - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.scm.shortcutreadersdk;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
